5541c6adf8dcc09cce5b111bb3b34034f956b7d3
hbdgr <hbdgr@protonamil.com>
2026-02-24 12:00:37 +0100

    ignition: new Test USD strategy deployment, new facets after diamondCut

---
+++ ignition/modules/test/TestUSDStrategy.ts
@@ -0,0 +1,68 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+import { parseEther, parseUnits } from "ethers";
+
+const TOKEN_SUPPLY = parseUnits("21000000", 6);
+const STRATEGY_SUPPLY = parseUnits("1000000", 6); // pre-supplied reserve strategy
+
+const TestUsdStrategyModule = buildModule("TestUsdStrategyModule", (m) => {
+  const diamond = m.getParameter("diamond", "0x");
+  const assetPrice = m.getParameter("assetPrice", parseEther("1"));
+
+  const strategyManager = m.getAccount(3);
+
+  // --- deploy USD stake token
+
+  const usdStakeToken = m.contract("TestERC20", [
+    "USD Stake Token",
+    "tUSD",
+    TOKEN_SUPPLY,
+    6,
+  ], { id: "UsdStakeToken" });
+
+  // --- deploy USD yield token (what strategy returns as allocation)
+
+  const usdYieldToken = m.contract("TestERC20", [
+    "USD Yield Token",
+    "tUSDy",
+    TOKEN_SUPPLY,
+    6,
+  ], { id: "UsdYieldToken" });
+
+  // --- deploy mock reserve strategy: stake=tUSD, asset=tUSDy, price=1:1
+
+  const impl = m.contract("MockReserveStrategy", [], { id: "impl" });
+
+  const initCalldata = m.encodeFunctionCall(impl, "initialize", [
+    diamond,
+    { token: usdStakeToken },
+    usdYieldToken,
+    assetPrice,
+  ]);
+
+  const proxy = m.contract("ERC1967Proxy", [impl, initCalldata]);
+  const reserveStrategy = m.contractAt("MockReserveStrategy", proxy);
+
+  // --- supply yield tokens to strategy
+
+  // mint yield tokens to strategyManager
+  m.call(usdYieldToken, "mint", [strategyManager, STRATEGY_SUPPLY], {
+    id: "MintYieldTokensToStrategyManager",
+    from: m.getAccount(0),
+  });
+
+  // approve strategy to pull yield tokens
+  m.call(usdYieldToken, "approve", [proxy, STRATEGY_SUPPLY], {
+    id: "ApproveYieldTokensToStrategy",
+    from: strategyManager,
+  });
+
+  // supply revenue asset into strategy reserve
+  m.call(reserveStrategy, "supplyRevenueAsset", [STRATEGY_SUPPLY], {
+    id: "SupplyRevenueAsset",
+    from: strategyManager,
+  });
+
+  return { usdStakeToken, usdYieldToken, proxy, reserveStrategy };
+});
+
+export default TestUsdStrategyModule;
